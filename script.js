d1099ab18e65e38c0ad8e22ee2b996476c7bb962
asqarovshodiyor <asqarovshodiyor71@gmail.com>
2021-07-12 13:47:11 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let x = 9342258.6
+let y = prompt("sizning hisobingizda qancha pul bor");
+let text = document.querySelector("#true");
+if (x <= y){
+    text.textContent = ("oq yo'l :) ");
+}
+else{
+    text.textContent = ("pulingiz yitmaydi ozgina sabr qiling");
+}
